a6bcacab3a06bd39def14c03f21b72546d4885c4
KhoiNguyen <thorwaitson@gmail.com>
2022-03-23 13:00:45 -0500

    update UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,37 @@
+// Declare DOM elements
 const form = document.querySelector(".needs-validation");
 
-form.addEventListener("keyup", (event) => {
+// Declare functions
+const validateForm = () => {
   form.classList.add("was-validated");
+
   document.querySelector("#success").style.display = "none";
-});
+  document.querySelector("#warn").style.display = "none";
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  event.stopPropagation();
-
-  form.classList.add("was-validated");
-
-  if (form.checkValidity()) {
-    form.reset();
-    form.classList.remove("was-validated");
-    document.querySelector("#success").style.display = "block";
-  }
-});
+  if (document.getElementsByName("breakfast-checked")[0].checked)
+    document.querySelector(".breakfast-dropdown").style.display = "block";
+  if (document.getElementsByName("breakfast-checked")[1].checked)
+    document.querySelector(".breakfast-dropdown").style.display = "none";
+};
 
 $(function () {
+  form.addEventListener("change", validateForm);
+  form.addEventListener("keyup", validateForm);
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    form.classList.add("was-validated");
+
+    if (form.checkValidity()) {
+      form.reset();
+      form.classList.remove("was-validated");
+      document.querySelector("#success").style.display = "block";
+    } else {
+      document.querySelector("#warn").style.display = "block";
+    }
+  });
+
   $("#check-in-date").datepicker();
   $("#check-out-date").datepicker();
 });
