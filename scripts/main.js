c3d66594779009650668f707b4cfb47d47fe6f14
KhoiNguyen <thorwaitson@gmail.com>
2022-04-05 13:01:04 -0500

    update UI

--- scripts/main.js
+++ scripts/main.js
@@ -21,21 +21,31 @@ $(function () {
     event.preventDefault();
     event.stopPropagation();
 
-    form.classList.add("was-validated");
+    const checkInDate = new Date($("#check-in-date").datepicker("getDate"));
+    const checkOutDate = new Date($("#check-out-date").datepicker("getDate"));
+
+    if (!(checkOutDate > checkInDate)) {
+      document.querySelector("#warn").innerText =
+        "Check-out date should be later than check-in date";
+      return (document.querySelector("#warn").style.display = "block");
+    }
 
     if (form.checkValidity()) {
       form.reset();
       form.classList.remove("was-validated");
       document.querySelector("#success").style.display = "block";
     } else {
+      document.querySelector("#warn").innerText = "Invalid form";
       document.querySelector("#warn").style.display = "block";
     }
   });
 
   $("#check-in-date").datepicker();
   $("#check-out-date").datepicker();
+
   $("#clear").click(() => {
     form.classList.remove("was-validated");
+    document.querySelector(".breakfast-dropdown").style.display = "none";
     form.reset();
   });
 });
